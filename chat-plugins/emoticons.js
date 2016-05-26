7d9e1cc5dcb868f199d30d7177893822ad8d1081
LegacyEcho <immortal3dfilms@gmail.com>
2016-05-27 00:44:23 +0100

    Setup: New emote

--- chat-plugins/emoticons.js
+++ chat-plugins/emoticons.js
@@ -162,6 +162,7 @@ if (typeof demFeels.extendEmotes === 'function') {
 	'yayface': 'http://i.imgur.com/anY1jf8.png',
 	'yesface': 'http://i.imgur.com/k9YCF6K.png',
 	'youdontsay': 'http://r32.imgfast.net/users/3215/23/26/64/smiles/280467785.jpg',
+	'feelsmoa': 'http://i.imgur.com/943QIfz.jpg?1',
 	});
 }
 
