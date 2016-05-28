2053f4f34cdfc061a6056269716e105319b8c972
unlockedgoals <itsjakeex3@icloud.com>
2016-05-28 15:57:37 -0400

    Create title.js

---
+++ chat-plugins/title.js
@@ -0,0 +1,54 @@
+/**
+  * Title plugin for showdown that appears in the profile while doing /profile
+   * Plugin developed by ~RichardAdams
+   **/
+'use strict';
+
+let fs = require('fs');
+let path = require('path');
+
+// log when a title is set
+/*
+**Title Plugin Created by ~RichardAdams
+*/
+function logTitle(message) {
+	if (!message) return;
+	let file = path.join(__dirname, '../logs/about.txt');
+	let date = "[" + new Date().toUTCString() + "] ";
+	let msg = message + "\n";
+	fs.appendFile(file, date + msg);
+}
+
+exports.commands = {
+	givetitle: function (target, room, user) {
+		if (!this.can('forcewin')) return false;
+		if (!target || target.indexOf(',') < 0) return this.parse('/help givetitle');
+
+		let parts = target.split(',');
+		let username = toId(parts[0]);
+		let title = parts.slice(1).join(", ").trim();
+
+		if (!title.length || title.length > 100) return this.errorReply("The title must be between 1 and 100 characters long.");
+
+		Db('about').set(username, title);
+		
+		this.sendReply(username + " was given " + title + ". ");
+		if (Users.get(username)) Users(username).popup(user.name + " has given you the title: ||||" + title);
+		logTitle(username + " was given the title \"" + title + "\" by " + user.name + ".");
+	},
+	givetitlehelp: ["/givetitle [user], [title] - Give a user a Title."],
+
+
+	resettitle: function (target, room, user) {
+		if (!this.can('forcewin')) return false;
+		
+        let parts = target.split(',');
+		let username = toId(parts[0]);
+		let title = parts.slice(1).join(", ").trim();
+		Db('about').set(username, title);
+		this.sendReply(target + " title have been removed");
+		logTitle(user.name + " removed the Title of " + target + ".");
+	},
+	resettitlehelp: ["/resettitle [user] - Reset user's title"],
+
+};
