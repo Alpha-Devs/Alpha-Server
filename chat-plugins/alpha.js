f044a6879a655aa15f7ad4e74b4bf8cbe6b95326
unlockedgoals <itsjakeex3@icloud.com>
2016-05-28 17:09:03 -0400

    Update alpha.js

--- chat-plugins/alpha.js
+++ chat-plugins/alpha.js
@@ -4,7 +4,4 @@ exports.commands = {
   hc: function(target, room, user) {
     return this.parse('/hotpatch chat')
   }
-  cas: function(target,room,user) {
-    return this.parse('/customavatar set')
-  }
 }
