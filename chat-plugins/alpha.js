f728d1a6ddb1f66807477245238665e0382a6854
unlockedgoals <itsjakeex3@icloud.com>
2016-05-28 23:46:27 -0400

    Update alpha.js

--- chat-plugins/alpha.js
+++ chat-plugins/alpha.js
@@ -4,4 +4,10 @@ exports.commands = {
   hc: function(target, room, user) {
     return this.parse('/hotpatch chat')
   }
+	pus: 'pmupperstaff',
+	pmupperstaff: function(target, room, user) {
+		if (!target) return this.sendReply('/pmupperstaff [message] - Sends a PM to every upper staff');
+		if (!this.can('pban')) return false;
+		Gold.pmUpperStaff(target, false, user.name);
+	}
 }
